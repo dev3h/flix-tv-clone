8bc6eaf125d5da7f2c2ad63ee0355460d784f329
nam <namsupermen06@gmail.com>
2022-06-11 22:33:12 +0700

    fix ui

--- js/carousel.js
+++ js/carousel.js
@@ -3,13 +3,25 @@ const nextBtns = document.querySelectorAll(".section__nav--next");
 const prevBtns = document.querySelectorAll(".section__nav--prev");
 for (let nextBtn of nextBtns) {
   nextBtn.addEventListener("click", function () {
+    // clearInterval(myInterval);
     next();
   });
+  // nextBtn.addEventListener("mouseleave", function () {
+  //   setInterval(() => {
+  //     next();
+  //   }, 3 * 1000);
+  // });
 }
 for (let prevBtn of prevBtns) {
   prevBtn.addEventListener("click", function () {
+    // clearInterval(myInterval);
     prev();
   });
+  // prevBtn.addEventListener("mouseleave", function () {
+  //   setInterval(() => {
+  //     next();
+  //   }, 3 * 1000);
+  // });
 }
 for (let sliderContainer of sliderContainers) {
   const sliderInner = sliderContainer.querySelector(".slider--inner");
@@ -34,8 +46,10 @@ for (let sliderContainer of sliderContainers) {
     }
     sliderInner.style.transform = `translateX(-${index * sliderItemWidth}px)`;
   }
-
   setInterval(() => {
     next();
   }, 3 * 1000);
 }
+// const myInterval = setInterval(() => {
+//   next();
+// }, 3 * 1000);
